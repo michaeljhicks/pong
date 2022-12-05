0743430a5905b67d655224cb3931ddeb47632861
Michael Hicks <hicks.michael04@gmail.com>
2022-12-05 12:39:13 -0700

    detect when paddles miss ball

--- main.py
+++ main.py
@@ -36,5 +36,14 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    # detect when right paddle misses
+    if ball.xcor() > 380:
+        # game_is_on = False
+        ball.reset_position()
+
+    # detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 
 screen.exitonclick()
