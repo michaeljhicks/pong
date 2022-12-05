1180fdbfde5877bb35eec67fc53afb5643916bc7
Michael Hicks <hicks.michael04@gmail.com>
2022-12-05 09:57:26 -0700

    create paddle class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from turtle import Screen, Turtle
-# from paddle import Paddle
+from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor('black')
@@ -7,35 +7,18 @@ screen.setup(width=800, height=600)
 screen.title('Pong')
 screen.tracer(0)
 
-# paddle = Paddle()
-paddle = Turtle()
-paddle.shape('square')
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.color('white')
-# paddle.speed('fastest')
-paddle.penup()
-paddle.goto(350, 0)
-# paddle.refresh()
-
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 
 screen.listen()
-screen.onkey(go_up, 'Up')
-screen.onkey(go_down, 'Down')
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
-
 while game_is_on:
     screen.update()
-
 
 screen.exitonclick()
