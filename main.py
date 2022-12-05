99419a394803452448f77c6eea22c8e090cff51b
Michael Hicks <hicks.michael04@gmail.com>
2022-12-05 12:55:27 -0700

    add scoring functionality

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.05)
     screen.update()
     ball.move()
 
@@ -37,14 +37,16 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
-    # detect when right paddle misses
+    # detect when RIGHT paddle misses
     if ball.xcor() > 380:
         # game_is_on = False
         ball.reset_position()
+        scoreboard.l_point()
 
-    # detect when left paddle misses
+    # detect when LEFT paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 
 screen.exitonclick()
